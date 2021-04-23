f4ba9f2bfcc47e9894b4921f3ea47d4c19faaa83
Miriam Curiel <curiel.miriam@gmail.com>
2021-04-22 21:45:13 -0500

    initial commit

---
+++ poo-fundamentos/ejercicio1/Parimpar - copia.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Parimpar {
+
+   public static void main(String[] args){
+      Scanner entrada = new Scanner(System.in);
+      
+      //tipoDato nombrevariable = valorInicializado;
+      ParesImpares utilidad = new ParesImpares();
+      int arreglo[] = utilidad.getArreglo();
+       
+   
+      /*ParesImpares parimpar = new ParesImpares();
+      System.out.println("El arreglo de numero es:" + ParesImpares.arreglo);
+      
+      ParesImpares ObtenArreglo = new ParesImpares();
+      System.out.println("El arreglo de numero es:" + ParesImpares.ObtenArreglo);*/
+      
+     }
+}
